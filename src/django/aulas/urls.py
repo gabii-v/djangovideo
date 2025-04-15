36d7ed1bd79d595ce52d2df20b6a1ce08304d30b
gabii-v <maticreen@gmail.com>
2025-04-14 21:21:20 -0300

    class 10 - postgres

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import nuevoHello
+from .views import nuevoHello, bye, edad, primer_plantilla, segunda_plantilla, tercer_plantilla, cuarta_plantilla, crear_musico, crear_albun
 
 urlpatterns = [
     path('hello', nuevoHello),
+    path('bye', bye),
+    path('edad/<int:anios>/<int:futuro>', edad),
+    path('plantilla1', primer_plantilla),
+    path('plantilla2', segunda_plantilla),
+    path('plantilla3', tercer_plantilla),
+    path('plantilla4', cuarta_plantilla),
+    path('crearmusico/<nombre>/<apellido>/<instrumento>', crear_musico),
+    path('crearalbun/<nombre>/<int:estrellas>/<int:artist_id>', crear_albun),
 ]
--- src/django/aulas/views.py
+++ src/django/aulas/views.py
@@ -1,6 +1,93 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render
+from .models import Musician, Album
 
 # Create your views here.
 def nuevoHello(request): 
     return HttpResponse("Hola mundo!!!")
+
+def bye(request): 
+    return HttpResponse("Hasta luego!!!")
+
+def edad(request, anios, futuro):
+    incremento = futuro - date.today().year
+    cumplira = anios + incremento
+
+    mensaje = "En el año %d cumpliras %d años"%(futuro, cumplira)
+
+    return HttpResponse(mensaje)
+
+def primer_plantilla(request):
+    plantilla = """
+    <html>
+        <body>
+            <h2>
+                Hola {{ nombre }}! esta es mi primer plantilla!
+            </h2>
+        </body>
+    </html>
+    """
+    
+    tpl = Template(plantilla)
+
+    ctx = Context({
+        'nombre': 'Gabriel Matias'
+    })
+
+    mensaje = tpl.render(ctx)
+
+    return HttpResponse(mensaje)
+
+def segunda_plantilla(request):
+    
+    tpl = get_template("segunda_plantilla.html")
+
+    mensaje = tpl.render({
+        'nombre': 'Gabriel Matias',
+        'fecha_actual': date.today()
+    })
+
+    return HttpResponse(mensaje)
+
+def tercer_plantilla(request):
+    
+    return render(request, "tercer_plantilla.html", {
+        'nombre': 'Pedro Gómez',
+        'fecha_actual': date.today()
+    })
+
+class Empleado(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+def cuarta_plantilla(request):
+
+    empleado = Empleado ('Juan', 'Gómez')
+
+    laborables = ['Lunes', 'Martes', 'Miércoles', 'Jueves']
+    
+    return render(request, "cuarta_plantilla.html", {
+        'mi_empleado': empleado,
+        'fecha_actual': date.today(),
+        'dias_laborables': laborables
+    }) 
+
+def crear_musico(request, nombre, apellido, instrumento):
+    per=Musician(first_name=nombre, last_name=apellido, instrument=instrumento )
+    per.save()
+    mensaje = "Se creó el músico %s %s con id %d"%(per.first_name, per.last_name, per.id)
+
+    return HttpResponse(mensaje)
+
+def crear_albun(request, nombre, estrellas, artista_id):
+    art=Musician.objects.get(id=artista_id)
+    albun=Album(name=nombre, release_date=date.today(), num_stars=estrellas, artist=art)
+    albun.save()
+    mensaje = "Se creó el Albun %s del Artista %s con id %d"%(albun.name, art.last_name, albun.id)
+
+    return HttpResponse(mensaje)
